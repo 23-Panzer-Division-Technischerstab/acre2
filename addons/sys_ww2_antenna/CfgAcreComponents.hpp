35161d47c17d766330b6851ddba93cf0dbc00621
Shock117 <140351208+Shock117@users.noreply.github.com>
2026-06-30 10:25:55 -0400

    feat(antenna): old 2m basic antenna addon

---
+++ addons/sys_ww2_antenna/CfgAcreComponents.hpp
@@ -0,0 +1,28 @@
+//CfgAcreComponents.hpp
+
+class CfgAcreComponents {
+    class ACRE_ComponentBase;
+
+    class ACRE_BaseAntenna: ACRE_ComponentBase {
+        type = ACRE_COMPONENT_ANTENNA;
+        simple = 1;
+        polarization = VERTICAL_POLARIZE;
+        heightAG = AVERAGE_MAN_HEIGHT;
+        orient = 90; // in degrees off of flat plane
+        name = "Default Antenna";
+        shortName = "Default";
+        connector = ACRE_CONNECTOR_TNC;
+        height = 1.2; //meters
+        // binaryGainFile = QPATHTOF(binary\Thales_100cm_Whip_gain.aba);
+        compatibleRadios[] = {};
+    };
+
+    class ACRE_WW2_2M_VH : ACRE_BaseAntenna {
+        name = "2 Meter Whip Antenna";
+        shortName = "2M Whip";
+        connector = ACRE_CONNECTOR_TNC;
+        height = 2;
+        binaryGainFile = QPATHTOF(binary\2mFusprechf_gain.aba);
+        compatibleRadios[] = {"WW2_FUSPRECHF"};
+    };
+};
---
+++ addons/sys_ww2_antenna/script_component.hpp
@@ -0,0 +1,19 @@
+#define COMPONENT sys_ww2_antenna
+#define COMPONENT_BEAUTIFIED WW2 Antenna
+#include "\idi\acre\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_WW2_ANTENNA
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_WW2_ANTENNA
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SYS_ANTENNA
+#endif
+
+#include "\idi\acre\addons\main\script_macros.hpp"
+
+#include "\idi\acre\addons\sys_components\script_acre_component_defines.hpp"
